92e75e22953dcc1b72d14d5bb6ed27a3fef7394f
Larry Tenório <larry.lhtx@gmail.com>
2026-05-09 16:11:56 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,18 @@
 package application;
 
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+		
+		Seller seller = new Seller();
+		System.out.println(seller);
+		
 	}
 
 }
